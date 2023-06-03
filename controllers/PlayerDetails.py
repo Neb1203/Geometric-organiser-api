d60ca85028eb54491b14d6ecd1c0684092a84c3b
Neb1203 <127986060+Neb1203@users.noreply.github.com>
2023-06-03 19:22:51 +0100

    Add endpoint to get player details

--- controllers/PlayerDetails.py
+++ controllers/PlayerDetails.py
@@ -5,6 +5,24 @@ class PlayerDetails:
     app = FastAPI()
     db_connect = DBConnect()
 
+    def read(self, email, password):
+        qry = """SELECT JSON_OBJECT(
+            'id', id,
+            'email', email,
+            'user_id', userId,
+            'username', userName,
+            'password', password,
+            'email', email
+        ) FROM playerDetails
+        WHERE email=%s AND password = %s
+        LIMIT 1"""
+        data = (email, password)
+
+        self.db_connect.cursor.execute(qry, data)
+        json_result = self.db_connect.cursor.fetchall()
+        self.db_connect.cursor.reset()
+        return json_result
+
     def write(self, user_name, email, password):
         qry = "INSERT INTO playerDetails (userName, password, email) VALUES (%s, %s, %s)"
         data = (user_name, password, email)
