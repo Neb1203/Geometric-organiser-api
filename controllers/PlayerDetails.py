c06b9f986949891f058f4c24e1d2c1557c085014
Neb1203 <127986060+Neb1203@users.noreply.github.com>
2023-08-09 13:37:41 +0100

    Joined playerStatistics

--- controllers/PlayerDetails.py
+++ controllers/PlayerDetails.py
@@ -6,34 +6,46 @@ from controllers.hashSalt import hashSalt
 class PlayerDetails:
     app = FastAPI()
     db_connect = DBConnect()
-
-    def read(self, email, password):
-        qry = """SELECT JSON_OBJECT(
-            'id', id,
-            'email', email,
-            'token', token,
-            'username', userName,
-            'password', password,
-        ) FROM playerDetails
-        WHERE email=%s AND password = %s
-        LIMIT 1"""
+    def cloudLogin(self, email, password):
+        qry = """
+            SELECT JSON_OBJECT(
+                'id', pd.id,
+                'email', pd.email,
+                'token', pd.token,
+                'username', pd.userName,
+                'password', pd.password,
+                'settings_save', JSON_OBJECT(
+                    'id', s.ID,
+                    'music_volume', s.musicVolume,
+                    'sfx_volume', s.sfxVolume
+                ),
+                'playerstatistics', JSON_OBJECT(
+                    'id', p.ID,
+                    'time_played', p.timePlayed,
+                    'account_level', p.accountLevel,
+                    'total_completed_lines', p.totalCompletedLines,
+                    'rounds_played', p.roundsPlayed,
+                    'campaign_attempts', p.campaignAttempts,
+                    'campaign_completion_rate', p.campaignCompletionRate,
+                    'average_round_length', p.averageRoundLength,
+                    'high_score', p.highScore,
+                    'deaths', p.deaths,
+                    'average_score', p.averageScore
+                )
+            )
+            FROM playerDetails pd
+            LEFT JOIN settingsave s ON s.ID = pd._fk_settingsave
+            LEFT JOIN playerstatistics p ON p.ID = pd._fk_player_statistics
+            WHERE pd.email = %s AND pd.password = %s
+            LIMIT 1
+        """
         hashPassword = hashSalt(password)
         data = (email, hashPassword)
-
         self.db_connect.cursor.execute(qry, data)
         self.db_connect.cursor.reset()
 
-        row = self.db_connect.cursor.fetchone()  # Fetch the first row
+        return self.db_connect.cursor.fetchone()  # Fetch the first row
 
-        response = {}
-        if row is not None:
-            json_object = row[0]  # Retrieve the JSON object as a string
-            parsed_json = json.loads(json_object)  # Parse the JSON string into a Python object
-            response['username'] = parsed_json['username']
-            response['token'] = parsed_json['token']
-            return response
-        else:
-            return None
     def write(self, user_name, email, password):
         hexHash = hashSalt(password)
         token = secrets.token_urlsafe(48)
