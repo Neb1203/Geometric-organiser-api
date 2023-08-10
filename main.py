d49bf0e4280b0c4c9512b9168db1e994d96faa6d
Neb1203 <127986060+Neb1203@users.noreply.github.com>
2023-08-10 17:20:20 +0100

    Added validate function

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import os
 
 import uvicorn
+from controllers.Authenticate import Authenticate
 from controllers.PlayerDetails import PlayerDetails
+
 from fastapi import FastAPI
 
 app = FastAPI()
 player_details = PlayerDetails()
+authenticate = Authenticate()
+
+@app.post("/authenticate/")
+async def post_authenticate(email: str, password: str):
+    return authenticate.index(email, password)
+@app.get("/validate/")
+async def validation(sessionToken: str):
+    return authenticate.validate(sessionToken)
+
 @app.get("/player_details/")
 async def get_player_details(email: str, password: str):
-    return player_details.read(email, password)
+    return player_details.cloudLogin(email, password)
 @app.post("/player_details/")
 async def post_player_details(user_name: str, email: str, password: str):
     return player_details.write(user_name, email, password)
