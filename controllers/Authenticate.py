3fc54f8d63e845eec4d58a4560aaf7ad67cc5fe6
Neb1203 <127986060+Neb1203@users.noreply.github.com>
2023-08-10 17:20:20 +0100

    Added validate function

--- controllers/Authenticate.py
+++ controllers/Authenticate.py
@@ -3,12 +3,13 @@ from fastapi import FastAPI
 from controllers.hashSalt import hashSalt
 import secrets
 import requests
-import datetime
+from datetime import datetime, timedelta
 class Authenticate:
     app = FastAPI()
     db_connect = DBConnect()
 
     def index(self, email, password):
+        print("running", email, password)
         qry = """
                     SELECT ID
                     FROM playerDetails pd
@@ -27,12 +28,14 @@ class Authenticate:
             token = secrets.token_urlsafe(24)
 
             qry = "INSERT INTO session (loginDate, sessionToken) VALUES (%s, %s)"
-            data = (datetime.datetime.now(), token)
+            data = (datetime.now(), token)
 
             self.db_connect.cursor.execute(qry, data)
             self.db_connect.cnx.commit()
 
             self.db_connect.cursor.reset()
+            with open("tokens.txt", "a") as file:
+                file.write(token + "\n")
             return token
         else:
             return "Account doesn't exist"
@@ -43,3 +46,18 @@ class Authenticate:
         # if not logged in then return status code of 422
 
         # if logd in add a new record to session table
+    def validate(self, sessionToken):
+        qry = """
+            SELECT loginDate
+            FROM session
+            WHERE sessionToken = %s
+            LIMIT 1
+        """
+        self.db_connect.cursor.execute(qry, (sessionToken,))
+        self.db_connect.cursor.reset()
+        record = self.db_connect.cursor.fetchone()
+        print("validate", record)
+        print(record)
+        return record
+
+
